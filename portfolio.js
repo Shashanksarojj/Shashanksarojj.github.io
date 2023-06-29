ed8782e2eb836c500d3d7eba2bf9dc94a96b52d7
Shashanksarojj <shashankdw1704@gmail.com>
2023-06-29 18:35:18 +0530

    2nd commit

--- portfolio.js
+++ portfolio.js
@@ -9,15 +9,15 @@ menuIcon.onclick = () =>{
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-ScrollReveal({
-    reset:true,
-    distance:'80px',
-    duration:2000,
-    delay:200,
+// ScrollReveal({
+//     reset:true,
+//     distance:'80px',
+//     duration:2000,
+//     delay:200,
     
-    });
+//     });
     
-    ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
-    ScrollReveal().reveal('.home-img,.project-container',{origin:'bottom'});
-    ScrollReveal().reveal('.home-content h1 , .about-img',{origin:'left'});
-    ScrollReveal().reveal('.home-content p , .about-img',{origin:'right'});
+//     ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+//     ScrollReveal().reveal('.home-img,.project-container',{origin:'bottom'});
+//     ScrollReveal().reveal('.home-content h1 , .about-img',{origin:'left'});
+//     ScrollReveal().reveal('.home-content p , .about-img',{origin:'right'});
